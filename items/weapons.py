4d3c0d38699e3722918a3013eeca630df8ee0593
Nathan <nathanjuliannj@gmail.com>
2026-07-06 18:36:31 +0200

    load added in weapons.py

--- items/weapons.py
+++ items/weapons.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+import json
+from pathlib import Path
 
 if TYPE_CHECKING:
     from entities.player import Player
+
+root: Path = Path(__file__).parent.parent
 
 class Weapon:
     def __init__(self, player: Player, wep_data: dict):
@@ -17,3 +21,14 @@ class Weapon:
         stat_amount: int = getattr(self.player.stats, self.scaling["stat"])
         scale: float = self.scaling["scale"]
         return self.base_atk + (stat_amount * scale)
+
+    @classmethod
+    def load(cls, player: Player, wep_id):
+        weapons_path: Path = root / "data" / "items" / "weapons.json"
+        with open(weapons_path) as f:
+            weapons_data: dict = json.load(f)
+
+        if wep_id not in weapons_data:
+            raise ValueError(f"Unknown weapon id: {wep_id}")
+
+        return cls(player, wep_id["id"])
