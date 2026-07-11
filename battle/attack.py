d2ca4fe5e50b308bf465cd1bcb8c355a270a366b
Nathan <nathanjuliannj@gmail.com>
2026-07-12 01:01:53 +0200

    add accuracy in attacks

--- battle/attack.py
+++ battle/attack.py
@@ -1,12 +1,17 @@
+import random
+
 class Attack:
     def __init__(self, data: dict):
         self.atk_id = data["id"]
         self.name = data["name"]
         self.damage = data["damage"]
         self.cost = data["cost"]
+        self.accuracy = data["accuracy"]
 
     def execute(self, attacker, target):
-        attacker.mp -= max(0, attacker.mp - self.cost)
-        target.take_damage(self.damage)
-        print(f"~ {attacker.name} used {self.name} on {target.name} for {self.damage}!")
-
+        attacker.mp = max(0, attacker.mp - self.cost)
+        if random.random() <= self.accuracy:
+            target.take_damage(self.damage)
+            print(f"~ {attacker.name} used {self.name} on {target.name} for {self.damage}!")
+        else:
+            print(f"~ {attacker.name} used {self.name} on {target.name} and missed!")
